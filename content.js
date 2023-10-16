7acfc98fc7df6f6402d1578546d925d16a3289c4
Vianney Veremme <10519369+Vianpyro@users.noreply.github.com>
2023-10-16 18:47:52 -0400

    Added auto-resume and comments

--- content.js
+++ content.js
@@ -1,23 +1,38 @@
+// This function removes "tp-yt-paper-dialog" elements from the DOM, which are typically YouTube Ad Paper dialogs.
 function removeTpYtPaperDialog() {
     const tpYtPaperDialog = document.querySelectorAll("tp-yt-paper-dialog");
+
     tpYtPaperDialog.forEach(dialog => {
-        console.log("Youtube Ad Paper removed successfuly!");
-        dialog.remove();
+        console.log("Youtube™ Ad Paper removed successfully!");
+        dialog.remove(); // Remove the dialog element from the DOM.
+
+        // Resume video playback.
+        document.dispatchEvent(playKeyEvent);
+        console.log("Resuming...");
     });
 }
 
-// MutationObserver to wait for changes in the DOM
+// Create a MutationObserver to monitor changes in the DOM.
 const observer = new MutationObserver(mutationsList => {
     for (const mutation of mutationsList) {
         if (mutation.addedNodes.length > 0) {
+            // If new nodes are added to the DOM, call the function to remove YouTube Ad Paper dialogs.
             removeTpYtPaperDialog();
         }
     }
 });
 
-// Start observing the DOM
+// Create a custom keyboard event to simulate pressing the 'k' key to play the video.
+const playKeyEvent = new KeyboardEvent("keydown", {
+    key: "k",
+    code: "KeyK",
+    keyCode: 32,
+    which: 32,
+    bubbles: true,
+    cancelable: true
+});
+
+// Start observing changes in the DOM, looking for child node additions and subtree changes.
 observer.observe(document, { childList: true, subtree: true });
 
-// Initial removal when the script is first injected
-console.log("Youtube Ad Paper Remover loaded!");
-removeTpYtPaperDialog();
+console.log("Youtube™ Ad Paper Remover loaded!");
